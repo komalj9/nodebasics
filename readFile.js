318dea4808dabb5fe6b7383bc21fa25688da2504
ketan pawar <ketan12pawar@gmail.com>
2024-02-23 08:48:51 +0530

    23/02/2024 code pushed

--- readFile.js
+++ readFile.js
@@ -3,13 +3,13 @@ const fs=require('fs');
 const port=8000;
 const filename="notes.txt";
 http.createServer(function(req, res){
-   fs.writeFile(`/home/administrator/Desktop/mern5Feb/${filename}`,"Morning mern stack class by durgesh sir",function(err, data){
-    console.log("error",err);
+   fs.readFile(`/home/administrator/Desktop/mern5Feb/${filename}`,'utf-8',function(err, data){
+    console.log("data",data);
        if(err){
         res.end(`${err.message}`);
        }
        else{
-        res.end(`the ${filename} file was created successfully and written something.`);
+        res.end(`the ${data} in the given file ${filename} ...`);
        }
    })
 }).listen(port,function(){
